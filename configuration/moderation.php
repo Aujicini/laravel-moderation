cc13b222d23e54fc8bedf3eeacea54f2f0917bea
oxuwazet <55375045+oxuwazet@users.noreply.github.com>
2020-06-03 01:51:02 -0400

    CS Fix and configuration location moved.

---
+++ configuration/moderation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Strict Moderation
+    |--------------------------------------------------------------------------
+    |
+    | If this is set to true then the default query will return only approved resources.
+    | In other case, all resources except Rejected ones, will be returned as well.
+    |
+    */
+
+    'strict' => true,
+
+);
