d426dd5ed23d61d28ac883b041de7ef37f2b0af7
Nicholas English <55375045+Aujicini@users.noreply.github.com>
2021-01-12 15:35:33 -0500

    Add moderation migration.

--- migrations/2021_01_03_000001_create_moderation_table.php
+++ migrations/2021_01_03_000001_create_moderation_table.php
@@ -1,1 +1,35 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModerationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void Returns nothing.
+     */
+    public function up()
+    {
+        Schema::create('moderation', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('updated_by');
+            $table->text('model')->default('user');
+            $table->foreignId('user_id');
+            $table->enum('status', ['approved', 'rejected', 'postponed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void Returns nothing.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moderation');
+    }
+}
