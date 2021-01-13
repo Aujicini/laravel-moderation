fceb833c485cdbc5d2b51f28cedd1d470b66fbb0
Nicholas English <oxuwazet@gmail.com>
2021-01-13 03:44:08 -0500

    Add the ipbans model.

--- src/Models/Ipbans.php
+++ src/Models/Ipbans.php
@@ -2,6 +2,18 @@
 
 namespace Aujicini\Moderation\Models;
 
-class Ipbans
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Ipbans extends Model
 {
+    /**
+     * Get the user that owns the ipban.
+     *
+     * @return mixed Returns the relationship.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
