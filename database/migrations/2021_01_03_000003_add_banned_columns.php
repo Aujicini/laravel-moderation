3ebd06232ccacc70f642346ae7438bf05278aa47
Nicholas English <oxuwazet@gmail.com>
2021-01-13 05:21:34 -0500

    Add the banned column.

---
+++ database/migrations/2021_01_03_000003_add_banned_columns.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBannedColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void Returns nothing.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('banned')->default(false)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void Returns nothing.
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('banned');
+        });
+    }
+}
