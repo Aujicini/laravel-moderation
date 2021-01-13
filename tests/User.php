887957639b8f595c06a6039cfe8cb9c5dacd2b55
Nicholas English <oxuwazet@gmail.com>
2021-01-13 05:02:36 -0500

    Add the test user model.

---
+++ tests/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Aujicini\Moderation\Test;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Aujicini\Moderation\Traits\Bannable;
+
+class User extends Model implements AuthorizableContract, AuthenticatableContract
+{
+    use Authenticatable;
+    use Authorizable;
+    use Bannable;
+
+    protected $fillable = ['email'];
+
+    public $timestamps = false;
+
+    protected $table = 'users';
+}
