524c3835f8020d08164f3486be06c933d824b9ae
Nicholas English <oxuwazet@gmail.com>
2021-01-14 03:34:21 -0500

    Add some assertions.

--- tests/BannableTest.php
+++ tests/BannableTest.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Aujicini\Moderation\Tests;
+
+class BannableTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function user_is_banned()
+    {
+        $user = User::find(1);
+        $this->assertTrue($user->canBan());
+        $this->assertTrue($user->bannable());
+        $this->assertTrue(!$user->isBanned());
+        $user->ban();
+        $this->assertTrue($user->isBanned());
+        $user->unban();
+        $this->assertTrue(!$user->isBanned());
+    }
+}
