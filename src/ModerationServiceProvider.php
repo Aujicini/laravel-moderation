202408e5f88b2c556da93e44c01f9c6536b0281c
Nicholas English <oxuwazet@gmail.com>
2021-01-14 05:16:40 -0500

    Fix the moderation service provider logic.

--- src/ModerationServiceProvider.php
+++ src/ModerationServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Oxuwazet\Moderation;
+namespace Aujicini\Moderation;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -9,21 +9,62 @@ class ModerationServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      *
-     * @return void
+     * @return void Returns nothing.
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../configuration/moderation.php' => config_path('moderation.php')
-        ], 'config');
+        $this->registerMigrations();
+        $this->registerPublishing();
     }
 
     /**
      * Register the application services.
      *
-     * @return void
+     * @return void Returns nothing.
      */
     public function register()
     {
+        $this->configure();
+    }
+
+    /**
+     * Setup the configuration for Moderation.
+     *
+     * @return void Returns nothing.
+     */
+    protected function configure()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/moderation.php', 'moderation'
+        );
+    }
+
+    /**
+     * Register the package migrations.
+     *
+     * @return void Returns nothing.
+     */
+    protected function registerMigrations()
+    {
+        if (Moderation::$runsMigrations && $this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        }
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void Returns nothing.
+     */
+    protected function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/moderation.php' => $this->app->configPath('cashier.php'),
+            ], 'moderation-config');
+            $this->publishes([
+                __DIR__.'/../database/migrations' => $this->app->databasePath('migrations'),
+            ], 'moderation-migrations');
+        }
     }
 }
