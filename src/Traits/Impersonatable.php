13d27d1bd7dc454c012076288b73e9c43f3128a2
Nicholas English <oxuwazet@gmail.com>
2021-01-15 16:10:54 -0500

    Add 2 helper methods.

--- src/Traits/Impersonatable.php
+++ src/Traits/Impersonatable.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace Aujicini\Moderation\Traits;
+
+trait Impersonatable
+{
+    /**
+     * Can this user ban other user models.
+     *
+     * @return bool Returns true if this user can ban and false if not.
+     */
+    public function canImpersonate()
+    {
+        return true;
+    }
+
+    /**
+     * Can this user be impersonated.
+     *
+     * @return bool Returns true if this user can ip ban and false if not.
+     */
+    public function canBeImpersonated()
+    {
+        return true;
+    }
+}
