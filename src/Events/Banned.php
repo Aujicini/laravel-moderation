05b9464601ab53d14e5e5fb30e1383309b08ca79
Nicholas English <oxuwazet@gmail.com>
2021-01-15 15:34:59 -0500

    Add the banned event.

--- src/Events/Banned.php
+++ src/Events/Banned.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Aujicini\Moderation\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Banned
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /** @var \Illuminate\Database\Eloquent\Model $user The user instance. */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $user The user being banned or unbanned.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(Model $user)
+    {
+        $this->user = $user;
+    }
+}
