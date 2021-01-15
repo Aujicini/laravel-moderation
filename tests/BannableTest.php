8429a41788f2dd8d17b4a7160b9046fe2cc677a6
Nicholas English <oxuwazet@gmail.com>
2021-01-15 15:45:39 -0500

    Test the events and the other 2 helper methods.

--- tests/BannableTest.php
+++ tests/BannableTest.php
@@ -2,6 +2,10 @@
 
 namespace Aujicini\Moderation\Test;
 
+use Aujicini\Moderation\Events\Banned;
+use Aujicini\Moderation\Events\Unbanned;
+use Illuminate\Support\Facades\Event;
+
 class BannableTest extends TestCase
 {
     /**
@@ -9,22 +13,43 @@ class BannableTest extends TestCase
      */
     public function user_is_banned()
     {
+        Event::fake();
         $user = User::find(1);
         $this->assertTrue($user->canBan());
         $this->assertTrue($user->bannable());
+        $this->assertTrue($user->canIpBan());
+        $this->assertTrue($user->ipBannable());
         $this->assertTrue(!$user->isBanned());
         $user->ban();
+        Event::assertDispatched(Banned::class, function ($event) use ($user) {
+            return $event->user === $user;
+        });
         $this->assertTrue($user->isBanned());
         $user->unban();
+        Event::assertDispatched(Unbanned::class, function ($event) use ($user) {
+            return $event->user === $user;
+        });
         $this->assertTrue(!$user->isBanned());
         $this->assertTrue(!$user->isIpBanned());
         $user->ban();
+        Event::assertDispatched(Banned::class, function ($event) use ($user) {
+            return $event->user === $user;
+        });
         $this->assertTrue(!$user->isIpBanned());
         $user->unban();
+        Event::assertDispatched(Unbanned::class, function ($event) use ($user) {
+            return $event->user === $user;
+        });
         $this->assertTrue(!$user->isIpBanned());
         $user->ban(true);
+        Event::assertDispatched(Banned::class, function ($event) use ($user) {
+            return $event->user === $user;
+        });
         $this->assertTrue($user->isIpBanned());
         $user->unban();
+        Event::assertDispatched(Unbanned::class, function ($event) use ($user) {
+            return $event->user === $user;
+        });
         $this->assertTrue(!$user->isIpBanned());
     }
 }
