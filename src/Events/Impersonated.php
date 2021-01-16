251944f38ae2d18b99c6f1ae4df5d7217c15011e
Nicholas English <oxuwazet@gmail.com>
2021-01-16 16:15:25 -0500

    Add impersonated event.

---
+++ src/Events/Impersonated.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Aujicini\Moderation\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Impersonated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /** @var \Illuminate\Database\Eloquent\Model $user The user instance. */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $user The user being banned or unbanned.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(Model $user)
+    {
+        $this->user = $user;
+    }
+}
