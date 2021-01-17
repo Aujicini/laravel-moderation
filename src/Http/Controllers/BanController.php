9c2e0af7235ab4c68ae7777f0345db19b031db22
Nicholas English <oxuwazet@gmail.com>
2021-01-16 19:57:39 -0500

    Add the ban controller.

--- src/Http/Controllers/BanController.php
+++ src/Http/Controllers/BanController.php
@@ -1,1 +1,78 @@
+<?php
 
+namespace Aujicini\Moderation\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
+
+class BanController extends BaseController
+{
+    /**
+     * Ban user.
+     *
+     * @param string $id The user to ban.
+     *
+     * @return \Illuminate\Http\RedirectResponse Returns the ban response.
+     */
+    public function ban($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            abort(403);
+        }
+        if (!$user->bannable()
+            || !Auth::user()->canBan($user)) {
+            abort(403);
+        }
+        $user->ban();
+        return $request->wantsJson()
+            ? new JsonResponse('', 200)
+            : redirect()->to(config('moderation.ban_location'));
+    }
+
+    /**
+     * Ipban user.
+     *
+     * @param string $id The user to ipban.
+     *
+     * @return \Illuminate\Http\RedirectResponse Returns the ban response.
+     */
+    public function ipban($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            abort(403);
+        }
+        if (!$user->ipBannable()
+            || !Auth::user()->canIpBan($user)) {
+            abort(403);
+        }
+        $user->ban(true);
+        return $request->wantsJson()
+            ? new JsonResponse('', 200)
+            : redirect()->to(config('moderation.ipban_location'));
+    }
+
+    /**
+     * Unban user.
+     *
+     * @param string $id The user to unban.
+     *
+     * @return \Illuminate\Http\RedirectResponse Returns the ban response.
+     */
+    public function unban($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            abort(403);
+        }
+        if (!$user->isBanned()) {
+            abort(403);
+        }
+        $user->unban();
+        return $request->wantsJson()
+            ? new JsonResponse('', 200)
+            : redirect()->to(config('moderation.unban_location'));
+    }
+}
