d31bb0660c7f82c4e5de9c4492af29df4e703510
Nicholas English <oxuwazet@gmail.com>
2021-01-17 17:04:16 -0500

    Add the ip address columns.

--- database/migrations/2021_01_02_000003_add_ip_columns.php
+++ database/migrations/2021_01_02_000003_add_ip_columns.php
@@ -1,1 +1,33 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBannedColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void Returns nothing.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->ipAddress('ip_address_register')->after('password')->nullable();
+            $table->ipAddress('ip_address_last')->after('password')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void Returns nothing.
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('ip_address_register', 'ip_address_last');
+        });
+    }
+}
